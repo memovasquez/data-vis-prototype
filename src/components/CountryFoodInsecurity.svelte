0a0ab1a9a2d54aae2b512b54a0e7c33b073ae540
Guillermo <guillermovasquez757@gmail.com>
2023-04-27 00:46:49 -0400

    Added slider with tickmarks below the bar chart

--- src/components/CountryFoodInsecurity.svelte
+++ src/components/CountryFoodInsecurity.svelte
@@ -14,7 +14,7 @@
     let xAxis;
     let yAxis;
     let svg;
-
+    let selectedYear = 2016;
 
     // set the dimensions and margins of the graph
     const margin = {top: 30, right: 30, bottom: 70, left: 60},
@@ -50,6 +50,44 @@
             .range([ height, 0]);
             yAxis = svg.append("g")
             .attr("class", "myYaxis");
+
+            // Add slider input
+            const slider = d3.select('#year-slider');
+            slider.attr('min', 2016)
+            .attr('max', 2021)
+            .attr('step', 1)
+            .attr('value', 2016)
+            .attr('list', 'tickmarks')
+            .on('input', () => {
+                selectedYear = +slider.node().value; // Convert to number
+                update(necessaryData, selectedYear);
+            });
+
+            const tickValues = d3.range(2016, 2022);
+
+            // const tickLabels = slider.append('div')
+            // .attr('class', 'tick-labels')
+            // .selectAll('div')
+            // .data(tickValues)
+            // .enter()
+            // .append('div')
+            // .attr('class', 'tick-label')
+            // .style('left', d => `${(d - 2016) / 5 * 100}%`)
+            // .text(d => d);
+
+
+
+            slider.append('datalist')
+            .attr('id', 'tickmarks')
+            .selectAll('option')
+            .data(tickValues)
+            .enter()
+            .append('option')
+            .attr('value', d => d)
+            .attr('label', d=>d);
+
+
+
     });
 
 
@@ -60,15 +98,15 @@
       
 
     // A function that create / update the plot for a given variable:
-    function update(data) {
+    function update(data, selected_year) {
         console.log("called update");
         // Parse the Data
         // X axis
         x.domain(data.map(d => d.country));
         xAxis.transition().duration(1000).call(d3.axisBottom(x));
         //filter by year
-        let selected_year = '2016';
-        data = data.filter((obj) => obj.year == selected_year);
+
+        data = data.filter((obj) => Number(obj.year) == selected_year);
 
         // Add Y axis
         y.domain([0, d3.max(data, d => d.percentFoodInsec) ]);
@@ -98,7 +136,7 @@
 
 
         if (necessaryData.length > 0){
-            update(necessaryData);
+            update(necessaryData, 2016);
         }
 
 
@@ -110,11 +148,13 @@
 
 <main>
 
-    <button on:click="{update(necessaryData)}">Variable 1</button>
-    <button on:click="{update(necessaryData)}">Variable 2</button>
+    <button on:click="{update(necessaryData,2016)}">Variable 1</button>
+    <button on:click="{update(necessaryData,2021)}">Variable 2</button>
     
     <!-- Create a div where the graph will take place -->
     <div id="my_dataviz"></div>
+    <input style="width: -webkit-fill-available;" id="year-slider" type="range" bind:value={selectedYear} />
+
 
 </main>
 
@@ -129,4 +169,17 @@
         color: var(--color-text);
         margin-top: 100px;
     }
+
+    .tick-labels {
+    position: relative;
+    height: 2em;
+    }
+  .tick-label {
+    position: absolute;
+    bottom: 0;
+    text-align: center;
+    width: 20%;
+    transform: translateX(-50%);
+  }
+
 </style>
