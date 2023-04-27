ca7438bf8069b1567d361f9b2124e31a66f61c88
Guillermo <guillermovasquez757@gmail.com>
2023-04-27 15:39:31 -0400

    Setting up for deployment on gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+// import {svelte} from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -9,3 +10,12 @@ export default defineConfig({
 		// emptyOutDir: true, // true if outDir is inside root. if outDir is not inside root, uncomment this.
 	  }
 });
+
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [svelte()],
+//   build: {
+//     outDir: './docs', // relative to index.html
+//     // emptyOutDir: true, // true if outDir is inside root. if outDir is not inside root, uncomment this.
+//   }
+// })
