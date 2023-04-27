9d79c25291dfa1dc4b5ded57a457bbb2dd344ecc
Guillermo <guillermovasquez757@gmail.com>
2023-04-27 19:25:06 -0400

    Updates

--- _app/immutable/chunks/1.3d9658b2.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.3b4cc6f3.js";export{t as component};
---
+++ _app/immutable/chunks/1.ee2e5db0.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.b7f35f0f.js";export{t as component};
