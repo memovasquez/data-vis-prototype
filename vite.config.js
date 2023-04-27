b1d7cec28f852e9fce380e3bde0ad1cd16f4cacc
Guillermo <guillermovasquez757@gmail.com>
2023-04-27 15:39:31 -0400

    Setting up for deployment on gh pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	base: "/foodsec-data-vis/",
-	plugins: [sveltekit()]
+	base: "/foodsec-data-vis",
+	plugins: [sveltekit()],
+	build: {
+		outDir: './docs', // relative to index.html
+		// emptyOutDir: true, // true if outDir is inside root. if outDir is not inside root, uncomment this.
+	  }
 });
