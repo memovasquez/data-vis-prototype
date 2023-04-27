b32cf2f2599cfff492d6607240afc8331f0206b1
Guillermo <guillermovasquez757@gmail.com>
2023-04-27 15:39:31 -0400

    Setting up for deployment on gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: dev ? '' : '/foodsec-data-vis',
+			base: dev ? '' : '/foodsec-data-vis/',
 		}
 	}
 };
