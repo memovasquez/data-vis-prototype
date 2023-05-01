ecdfe23b98e157ad59a4694ede393a28383c6685
Guillermo <guillermovasquez757@gmail.com>
2023-05-01 16:38:02 -0400

    Trying docs

---
+++ docs/_app/immutable/chunks/1.261d8e74.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.ed38b1bf.js";export{t as component};
---
+++ docs/_app/immutable/chunks/3.3faa4073.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/fourth_slide-page.svelte.2bd5e446.js";export{t as component};
