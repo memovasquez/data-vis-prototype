30ce23078e101deee16346a2fff04a621e44ae46
Guillermo <guillermovasquez757@gmail.com>
2023-05-02 17:34:47 -0400

    changes in hopes of deploying statically to gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -5,14 +5,14 @@ import { vitePreprocess } from "@sveltejs/kit/vite";
 const dev = process.argv.includes("dev");
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
+	preprocess: vitePreprocess(),
+	base: "/foodsec-data-vis/",
+	kit: {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
-    paths: {
-      base: dev ? "" : "/foodsec-data-vis/",
-    },
+    
   },
 
   preprocess: [
--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	base: "/foodsec-data-vis",
 	plugins: [sveltekit()],
-	build: {
-		outDir: './docs', // relative to index.html
-		// emptyOutDir: true, // true if outDir is inside root. if outDir is not inside root, uncomment this.
-	  }
 });
 
 // https://vitejs.dev/config/
